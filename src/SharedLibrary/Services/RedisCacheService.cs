b5a68e4231e57d80c247fcec7117302a3a236caa
Alexander Vincent <16097639+f2calv@users.noreply.github.com>
2021-03-13 05:23:57 +0100

    redis ConfigurationOptions rename

--- src/SharedLibrary/Services/RedisCacheService.cs
+++ src/SharedLibrary/Services/RedisCacheService.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Threading.Tasks;
-using CasCap.Extensions;
+﻿using CasCap.Extensions;
 using Microsoft.Extensions.Logging;
 using StackExchange.Redis;
+using System;
+using System.Threading.Tasks;
 namespace CasCap.Services
 {
     public class RedisCacheService
@@ -12,18 +12,19 @@ namespace CasCap.Services
         public RedisCacheService(ILogger<RedisCacheService> logger)
         {
             _logger = logger;
-            _configurationOptions = ConfigurationOptions.Parse("localhost:6379");//or add hosts entry for redis-svc->127.0.0.1
-            _configurationOptions.ConnectRetry = 20;
-            _configurationOptions.ClientName = Environment.MachineName;
+            configuration = ConfigurationOptions.Parse("localhost:6379");//or add hosts entry for redis-svc->127.0.0.1
+            configuration.ConnectRetry = 20;
+            configuration.ClientName = $"{AppDomain.CurrentDomain.FriendlyName}-{Environment.MachineName}";
+            //configuration.AllowAdmin = true;
             //Note: below for getting redis working container to container on docker compose, https://github.com/StackExchange/StackExchange.Redis/issues/1002
             //_configurationOptions.ResolveDns = bool.TryParse(Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_COMPOSE"), out var _);
 
             //LuaScripts = GetLuaScripts();
         }
 
-        static ConfigurationOptions _configurationOptions { get; set; } = new();
+        static ConfigurationOptions configuration { get; set; } = new();
 
-        readonly Lazy<ConnectionMultiplexer> LazyConnection = new(() => ConnectionMultiplexer.Connect(_configurationOptions));
+        readonly Lazy<ConnectionMultiplexer> LazyConnection = new(() => ConnectionMultiplexer.Connect(configuration));
 
         ConnectionMultiplexer Connection { get { return LazyConnection.Value; } }
 
